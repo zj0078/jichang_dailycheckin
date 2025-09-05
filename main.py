4915ca29551d0ea0e046758f6aa57d0fa3463a1c
zj0078 <zj0078@qq.com>
2025-09-05 12:25:25 +0800

    Update main.py
    修改域名

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def push(content):
 
 # 会不定时更新域名，记得Sync fork
 
-ikuuu_url= 'https://ikuuu.ch'
+ikuuu_url= 'https://ikuuu.de'
 
 login_url = ikuuu_url+'/auth/login'
 check_url = ikuuu_url+'/user/checkin'
